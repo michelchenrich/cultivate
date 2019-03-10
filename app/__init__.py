26a92e85aecc193cc1c8dca8e73a4e6eb1aea87e
Michel Henrich <michelchenrich@gmail.com>
2019-03-10 15:57:15 -0300

    Add unicode decoding for words with umlauts

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+from unidecode import unidecode
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from tatoeba import scraper
@@ -11,7 +12,7 @@ class Sentence(Resource):
         parser.add_argument("target_language")
         arguments = parser.parse_args()
 
-        tatoeba = scraper.TatoebaScraper(arguments["word"], arguments["source_language"], arguments["target_language"])
+        tatoeba = scraper.TatoebaScraper(unidecode(arguments["word"]), arguments["source_language"], arguments["target_language"])
         return tatoeba.get_sentences(), 200
 
 app = Flask(__name__)
---
+++ run.sh
@@ -0,0 +1,1 @@
+venv/bin/gunicorn app:app -b 127.0.0.1:7000
